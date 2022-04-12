ac107a7e52e06dfd0e723934d7e485df073072ce
Rafael (Carvalho) Santos <rsantos@pop-os.localdomain>
2022-04-12 19:33:50 -0300

    feat(Token): Implementado o Token

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,10 @@
 const userService = require('../services/user');
+const { createToken } = require('../utils/token');
 
 const create = async (req, res) => {
   const user = await userService.create(req.body);
-  return res.status(201).json(user);
+  const token = createToken(user);
+  return res.status(201).json({ token });
 };
 
 module.exports = {
