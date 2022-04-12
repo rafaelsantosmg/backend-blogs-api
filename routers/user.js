15d37f10609b08ff5f07db2c19cd85b0bcf63d79
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-04-12 17:20:16 -0300

    feat(UserRouter) Implementado a Rota User

---
+++ routers/user.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const rescue = require('express-rescue');
+const validateJoi = require('../middlewares/validateJoi');
+const { validateUser } = require('../schemas/JoiSchemas');
+// const validateJWT = require('../auth/validateJWT');
+const { create } = require('../controllers/user');
+
+router.post('/', validateJoi(validateUser), rescue(create));
+
+module.exports = router;
